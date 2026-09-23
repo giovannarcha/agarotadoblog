a255c8225a2161d092ea7ed07d52d8de3f774e6d
giovannarcha <giovanna10@gmail.com>
2026-09-23 20:24:04 -0300

    feature 2: validacao de tamanho minimo do conteudo do post

--- blog/forms.py
+++ blog/forms.py
@@ -11,3 +11,16 @@ class ComentarioForm(forms.ModelForm):
             'texto': forms.Textarea(attrs={'placeholder': 'Deixe seu comentário...', 'rows': 4}),
         }
 
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['titulo', 'conteudo', 'imagem_capa', 'categoria', 'tags']
+
+    def clean_conteudo(self):
+        conteudo = self.cleaned_data.get('conteudo')
+        if conteudo and len(conteudo) < 50:
+            raise forms.ValidationError(
+                'O conteúdo precisa ter pelo menos 50 caracteres. Escreva um pouco mais antes de salvar!'
+            )
+        return conteudo
